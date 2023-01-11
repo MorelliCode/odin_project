b1d04b17a9db8fb3c95e31c868df3bc9786e676a
MorelliCode <pietro.morelli@gmail.com>
2023-01-11 13:21:50 -0300

    Add many files from "npm install"

--- Javascript-exercises-2/04_removeFromArray/removeFromArray.js
+++ Javascript-exercises-2/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
 const removeFromArray = function() {
+    let newArray = arguments[0];
+    for (i = 1; i <= arguments.length ; i++){
+        for (j = 0 ; j < newArray.length ; j++){
+            if (newArray[j] == arguments[i]){
+                newArray.slice(j, 1);
+            }
+        }
 
+    }
+    
+    return newArray;
 };
 
 // Do not edit below this line
