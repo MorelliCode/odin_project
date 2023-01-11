6021291a66ce544c999d1e550e63b7fa27a7238c
Pietro <pietro.morelli@gmail.com>
2023-01-11 08:28:50 -0300

    Complete exercise 03 of Javascript-exercises-2.

--- Javascript-exercises-2/03_reverseString/reverseString.js
+++ Javascript-exercises-2/03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(originalString) {
+    let newString = "";
+    for (let i = originalString.length; i > 0; i--){
+        newString += originalString.slice(i - 1, i);
+    }
 
+    return newString;
 };
 
 // Do not edit below this line
