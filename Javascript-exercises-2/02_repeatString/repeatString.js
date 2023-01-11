bbb29180018e82a30e7e6cf32967c972d750e498
Pietro <pietro.morelli@gmail.com>
2023-01-11 08:03:06 -0300

    Finish exercise 02 from Javascript-exercises-2.

--- Javascript-exercises-2/02_repeatString/repeatString.js
+++ Javascript-exercises-2/02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(originalString, repetitionNumber) {
+    if (repetitionNumber < 0){
+        return "ERROR";
+    }
 
+    let newString = "";
+    for (let i=0; i < repetitionNumber; i++){
+        newString += originalString;
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
