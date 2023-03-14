d046db41f4df7871ef254d3381121209010c7397
Pietro <pietro.morelli@gmail.com>
2023-03-14 15:01:42 -0300

    Solve exercise 10.

--- Javascript-exercises-2/10_fibonacci/fibonacci.js
+++ Javascript-exercises-2/10_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num <= 0){
+        return 'OOPS';
+    }
+
+    let fibonacci = [];
+
+    for(let i = 0 ; i < num ; i++){
+        if (i === 0 || i === 1){
+            fibonacci.push(1);
+            continue;
+        };
+        fibonacci.push(fibonacci[i-1] + fibonacci[i-2]);
+    };
+    return fibonacci[num-1];
 
 };
 
