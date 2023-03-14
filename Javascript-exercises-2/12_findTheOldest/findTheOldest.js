530ccfe9aad5d27356e8e58f035ab4c5ea4c7f36
Pietro <pietro.morelli@gmail.com>
2023-03-14 16:46:52 -0300

    Solve exercise 12.

--- Javascript-exercises-2/12_findTheOldest/findTheOldest.js
+++ Javascript-exercises-2/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const getAge = (birth, death) => {
+        if(!death){
+            death = new Date().getFullYear();
+        };
+        return death - birth;
+    };
+    
+    const sortedList = people.sort((a, b) => {
+        ageA = getAge(a.yearOfBirth, a.yearOfDeath);
+        ageB = getAge(b.yearOfBirth, b.yearOfDeath);
+        return ageA > ageB ? -1 : 1;
+    });
+    return sortedList[0];
 };
 
 // Do not edit below this line
