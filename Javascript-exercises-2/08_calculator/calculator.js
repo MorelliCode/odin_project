a48e518846c130d57bfae17a400c5cbffa2ae15e
Pietro <pietro.morelli@gmail.com>
2023-03-14 13:38:22 -0300

    Complete exercise 08 from Odin Javascript exercises.

--- Javascript-exercises-2/08_calculator/calculator.js
+++ Javascript-exercises-2/08_calculator/calculator.js
@@ -6,20 +6,34 @@ const subtract = function(num1, num2) {
 	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	const total = nums.reduce((sumTotal, num) => {
+    return sumTotal + num;
+  },0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  const total = nums.reduce((multTotal, num) => {
+    return multTotal * num;
+  });
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	let pwrTotal = 1;
+  for(let i = 0; i < num2; i++){
+    pwrTotal *= num1;
+  }
+  return pwrTotal;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let totalFac = 1;
+  for( let i = 1 ; i <= num ; i++){
+    totalFac *= i;
+  };
+  return totalFac;
 };
 
 // Do not edit below this line
