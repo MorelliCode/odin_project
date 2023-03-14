dfb8aafc6dedd465119d17d71e080d54ef9bfd48
Pietro <pietro.morelli@gmail.com>
2023-03-14 14:33:26 -0300

    Solve exercise 09.

--- Javascript-exercises-2/09_palindromes/palindromes.js
+++ Javascript-exercises-2/09_palindromes/palindromes.js
@@ -1,6 +1,6 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (text) {
+    let processedString = text.toLowerCase().replace(/[^a-z]/g, '');
+    return processedString.split('').reverse().join('') == processedString;
+}
 // Do not edit below this line
 module.exports = palindromes;
