710ce0b4f764a219f3f3159e8b3c6ec249b5b72c
MorelliCode <110130671+MorelliCode@users.noreply.github.com>
2024-07-31 15:56:36 -0300

    Add renderPage function.

--- objects-exercises/project-library.js
+++ objects-exercises/project-library.js
@@ -1,3 +1,5 @@
+const libraryElement = document.querySelector(".library");
+
 const myLibrary = [];
 
 function Book(title, author, numberPages, isRead) {
@@ -22,3 +24,59 @@ myLibrary.push(theHobbit);
 
 
 
+function addBook() {
+    let title = document.querySelector("#book-title").value;
+    let author = document.querySelector("#book-author").value;
+    let pages = document.querySelector("#book-pages").value;
+    let read = document.querySelector("#book-read").value;
+
+    let newBook = new Book(title, author, pages, read);
+
+    myLibrary.push(newBook);
+
+    renderPage();
+}
+
+function renderPage(){
+    document.querySelector(".library").innerHTML = "";
+
+    myLibrary.forEach(element => {
+        const card = document.createElement("div");
+        const cardTitle = document.createElement("div");
+        const cardAuthor = document.createElement("div");
+        const cardPages = document.createElement("div");
+        const cardRead = document.createElement("div");
+        const cardToggleBtn = document.createElement("button");
+        const cardRemoveBtn = document.createElement("button");
+
+        card.classList.add("card")
+        cardTitle.classList.add("card-title")
+        cardAuthor.classList.add("card-author")
+        cardPages.classList.add("card-pages")
+        cardRead.classList.add("card-read");
+
+        cardTitle.textContent = element.title;
+        cardAuthor.textContent = element.author;
+        cardPages.textContent = element.numberPages;
+        
+        if (element.isRead = true) {
+            cardRead.textContent = "Read";
+        }
+        if (element.isRead = false) {
+            cardRead.textContent = "Unread";
+        }
+
+        cardToggleBtn.textContent = "Toggle read";
+        cardRemoveBtn.textContent = "Remove";
+
+        card.appendChild(cardTitle);
+        card.appendChild(cardAuthor);
+        card.appendChild(cardPages);
+        card.appendChild(cardRead);
+        card.appendChild(cardToggleBtn);
+        card.appendChild(cardRemoveBtn);
+        document.querySelector(".library").appendChild(card);
+        
+    });
+
+}
