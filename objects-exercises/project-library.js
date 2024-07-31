44808423d20a55bd4e768a2c9cf86e4fe66f800d
MorelliCode <110130671+MorelliCode@users.noreply.github.com>
2024-07-31 11:26:01 -0300

    Create toggleRead function.

--- objects-exercises/project-library.js
+++ objects-exercises/project-library.js
@@ -5,12 +5,12 @@ function Book(title, author, numberPages, isRead) {
     this.author = author;
     this.numberPages = numberPages;
     this.isRead = isRead;
-    this.info = function() { 
-        return(this.title+" by "+this.author+", "+this.numberPages+" pages, "+this.isRead+".") 
+    this.toggleRead = function() { 
+        this.isRead = !this.isRead; 
     };
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
 
 myLibrary.push(theHobbit);
 
